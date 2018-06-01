cf9c246c5634e7a04e12cba2493b5a28af39d64c
Víctor Cordero <victorcordero100@gmail.com>
2018-06-01 11:48:11 +0200

    Initial version after many tries.

---
+++ home-credit-default-risk/model.py
@@ -0,0 +1,177 @@
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from utils import split_num_str_data, drop_nan_by_thresh, estimate_auc, report_classification, preprocess_test_set,\
+    deal_with_nan, encode_categorical_variables, get_most_important_features
+from sklearn.model_selection import train_test_split, GridSearchCV
+from imblearn.over_sampling import SMOTE, ADASYN
+from xgboost import XGBClassifier
+import pandas as pd
+import random
+import os
+import time
+
+# TODO
+# 0 - Mirar algun cruce de tablas por el sk_curr_id
+# 1 - Categoricas que son False y True pasarlas a unos y ceros
+# 2 - Categoricas que sean cadenas (One hot Encoding?)
+# 3 - Una vez mirado lo anterior, quitamos lo nulos(eliminar filas y columnas).
+# 4 - Despues imputamos(moda. media? Revisar con cuidado cuando son cadenas)
+# 5 - Usar RFC para sacar la importancia de las features.
+# 6 - Quitar las no importantes
+# 6.5 - Sacar hiperparametros
+# 7 - Usar IsolationForest
+# 8 - Investigar como obtener el vector de probabilidades(hay un método?¿?)
+# 9 - Grid Search
+# 10 - Usar XGBoost
+# 11 - Quitar variables correladas
+
+
+ini_time = time.time()
+
+SEED = 1234
+TEST_SIZE = 0.2
+THRESH = 0.8
+NJOBS = -1
+TARGET = "TARGET"
+SK_ID_CURR = "SK_ID_CURR"
+USER = "ncarvalho"
+
+random.seed(SEED)
+
+# TODO Improves
+# Quitar oversampling y probar random forest con balanceo -> hecho, empeora mucho, tanto con "balanced" como metiendo a mano los pesos
+# Probar best params y entrenar con columnas relevantes -> hecho, al no coger todas las columnas mejora un poco pero al tener el balanceo anterior empeora
+# Train with 80 - 20 -> Hecho
+# Check features importance con el oversampling original de Nuno y ver si mejora un poco al quitarle features innecesarias -> Hecho
+# isolated forest -> no tiene predict_proba y devuelve 1 y -1. Mucho trabajo adaptarlo.
+# Probar xgboost -> tampoco mejora mucho y el predict_proba hay que cambiar los argumentos.
+# Revisar las metricas tras corregir lo del resampleo del test, en kaggle bien pero en local de pena.
+
+base_path = "/home/" + USER + "/.kaggle/competitions/home-credit-default-risk"
+print("Loading files")
+df_app_train = pd.read_csv(os.path.join(base_path, "application_train.csv"))
+df_app_test = pd.read_csv(os.path.join(base_path, "application_test.csv"))
+
+print("------------- Initial preprocessing ---------------")
+print("Positive class data:", sum(df_app_train[TARGET]))
+print("Total data :", df_app_train[TARGET].count())
+
+pos_weight = sum(df_app_train[TARGET]) / df_app_train[TARGET].count() * 100
+print("Positive class data percentage:", pos_weight)
+
+
+print("Columns before dropping column nan's", df_app_train.shape[1])
+
+# Removes columns
+df_app_train = drop_nan_by_thresh(df=df_app_train, thresh=THRESH, axis=1)
+
+print("Columns after dropping column nan's", df_app_train.shape[1])
+
+
+print("Rows before dealing with nan ",  df_app_train.shape[0])
+df_app_train = deal_with_nan(df=df_app_train, action="drop_rows")
+
+print("Rows after dealing with nan ", df_app_train.shape[0])
+
+# Split numeric and non numeric data
+df_app_train_num, df_app_train_str = split_num_str_data(df_app_train)
+
+print("Encoding categorical variables")
+df_app_train_str = encode_categorical_variables(df_app_train_str)
+df_app_train = pd.concat([df_app_train_num, df_app_train_str], axis=1)
+
+del df_app_train_num, df_app_train_str
+
+print("Removing variables with no variance")
+# Remove columns with no variance
+df_app_train = df_app_train.loc[:,df_app_train.apply(pd.Series.nunique) != 1]
+
+
+print("Separating predicter variables from target variable")
+# Separate predicter variables and target variable
+X = df_app_train.loc[:, ~ df_app_train.columns.isin([TARGET, SK_ID_CURR])]
+y = df_app_train[TARGET]
+
+
+print("Train/Test split")
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=TEST_SIZE,
+                                                    random_state=SEED, stratify=y)
+
+
+print("Train size before resampling", X_train.shape[0])
+
+
+print("SMOTE Oversampling ..")
+X_resampled, y_resampled = SMOTE(random_state=SEED, kind='regular').fit_sample(X_train, y_train)
+pos_weight_after = sum(y_resampled) / y_resampled.size * 100
+
+print("Positive class samples percentage after resampling", pos_weight_after)
+
+
+print("Train Size after resampling", X_resampled.shape[0])
+
+'''
+clf_opt = RandomForestClassifier(n_jobs=NJOBS,
+                                 random_state=SEED,
+                                 bootstrap=False,
+                                 max_depth=6,
+                                 min_samples_leaf=3,
+                                 min_samples_split=5,
+                                 n_estimators=200,
+                                 verbose=2)
+'''
+
+clf_opt = GradientBoostingClassifier(random_state=SEED,
+                                     max_depth=4,
+                                     min_samples_leaf=3,
+                                     min_samples_split=5,
+                                     n_estimators=200,
+                                     verbose=2)
+
+print("Fitting model ... ")
+clf_opt.fit(X_train, y_train)
+
+
+get_most_important_features(clf=clf_opt, columns=X_train.columns)
+
+
+print("Model fitted. Predicting ...")
+y_pred_test = clf_opt.predict_proba(X_test)
+y_pred_train = clf_opt.predict_proba(X_train)
+
+# Get the second element of each list. The second element is the probability of label 1.
+y_pred_test = list(map(lambda x: x[1], y_pred_test)) # [[0.14, 0.86],[0.23, 0.77],[0.35, 0.65]] -> [0.86, 0.77, 0.65]
+y_pred_train = list(map(lambda x: x[1], y_pred_train)) # [[0.14, 0.86],[0.23, 0.77],[0.35, 0.65]] -> [0.86, 0.77, 0.65]
+
+estimate_auc(y_pred=y_pred_train, y_test=y_train, name="Train")
+estimate_auc(y_pred=y_pred_test, y_test=y_test, name="Test")
+
+report_classification(y_test=y_train, predicted=clf_opt.predict(X_train))
+report_classification(y_test=y_test, predicted=clf_opt.predict(X_test))
+
+# Predict on test data
+
+print("Prepare test data for prediction")
+
+df_app_test = preprocess_test_set(df_train=df_app_train, df_test=df_app_test)
+
+print("Predict on test data")
+
+# Get the ID column
+sk_id_curr = df_app_test[SK_ID_CURR]
+
+# Remove the unique ID(PK) from the test data
+df_app_test = df_app_test.loc[:, df_app_test.columns != SK_ID_CURR]
+
+# Predict
+y_pred_test = clf_opt.predict_proba(df_app_test)
+
+# This line does this:  [[0.14, 0.86],[0.23, 0.77],[0.35, 0.65]] -> [0.86, 0.77, 0.65]
+y_pred_test_prob = list(map(lambda x: x[1], y_pred_test))
+
+print("Generating kaggle submision")
+kaggle_submission = pd.DataFrame({TARGET: y_pred_test_prob, SK_ID_CURR: sk_id_curr})
+kaggle_submission.to_csv("submission.csv", index=False)
+
+
+print("Total seconds = ", time.time() - ini_time)
---
+++ home-credit-default-risk/utils.py
@@ -0,0 +1,211 @@
+from sklearn.metrics import roc_curve, auc, classification_report
+from sklearn import preprocessing
+from sklearn.base import TransformerMixin
+from sklearn.model_selection import GridSearchCV
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def split_num_str_data(df):
+    '''
+
+    :param df: Dataframe that we want to split
+    :return: Returns two dataframes. The first is a dataframe which only contains numerical variables. The second
+    dataframe only contains string data.
+    '''
+
+    df_num = df.select_dtypes(include=['int64', 'float64'])
+    df_str = df.select_dtypes(include='object')
+    return df_num, df_str
+
+
+def preprocess_test_set(df_test, df_train):
+
+    '''
+
+    This function prepares the test set for the prediction.
+    This preparation includes:
+    1 - Keep the same columns as in the training set
+    2 - Impute null values
+    3 - Encode categorical vlaues
+
+    :param df: Test set dataframe
+    :return:
+    '''
+    print("Adjusting test data to predict over it")
+    # Keep the same columns
+    df_test = df_test.loc[:, df_test.columns.isin(df_train.columns)]
+
+    df_test = DataFrameImputer(fill_type="mean_mode").fit_transform(X=df_test)
+
+    df_test_num, df_test_str = split_num_str_data(df_test)
+
+    df_str = encode_categorical_variables(df_test_str)
+    df_num = pd.concat([df_test_num, df_str], axis=1)
+
+    return df_num
+
+def estimate_auc(y_test, y_pred, name=None):
+    '''
+    This function calculates de area under the roc curve.
+
+    :param y_test: Real labels([1, 0, 1, etc])
+    :param name: Name you want to give the file
+    :param y_pred: Predicted labels as a probability score([0.123678, 0.967, 0.1231, etc])
+    :return:
+    '''
+    fpr, tpr, thresholds = roc_curve(y_true=y_test, y_score=y_pred, pos_label=1)
+    roc_auc = auc(fpr, tpr)
+    print("AUC ", roc_auc)
+
+    fig = plt.figure()
+    plt.title('Receiver Operating Characteristic')
+    plt.plot(fpr, tpr, 'b', label='AUC = %0.5f' % roc_auc)
+    plt.legend(loc='lower right')
+    plt.plot([0, 1], [0, 1], 'r--')
+    plt.xlim([0, 1])
+    plt.ylim([0, 1])
+    plt.ylabel('True Positive Rate')
+    plt.xlabel('False Positive Rate')
+    fig.savefig('roc_curve_' + name + '_.png')
+
+def get_most_important_features(clf, columns):
+    '''
+
+    :param clf: Sklearn Classifier optimized with GridSearch
+    :param columns: list of columns
+    :return:
+    '''
+    feature_importances = clf.feature_importances_
+    print("Most important features are: ",
+          sorted(zip(map(lambda x: round(x, 4), feature_importances), columns), reverse=True))
+
+
+
+
+def report_classification(y_test, predicted):
+    report = classification_report(y_test, predicted)
+    print("Classification report: ", report)
+
+
+def encode_categorical_variables(df):
+    '''
+
+
+    :param df: Codifies each column of the dataframe with Label Encoder algorithm.
+
+     LabelEncoder acts in two steps:
+
+     1 - Fit:
+
+        'a' -> 1
+        'b' -> 2
+        'c' -> 3
+        'd' -> 4
+
+     2 - Transform:
+
+        ['a', 'a', 'b', 'c', 'a', 'd'] --> [1, 1, 2, 3, 1, 4]
+
+
+    :return: The encoded Dataframe
+    '''
+
+
+    le = preprocessing.LabelEncoder()
+    for col in df.columns:
+        le.fit(df[col])
+        df[col] = le.transform(df[col])
+    return df
+
+def drop_nan_by_thresh(df, thresh, axis):
+    shape = axis - 1 if axis == 1 else axis + 1
+    df = df.dropna(axis=axis, thresh=int(thresh*df.shape[shape]))
+    return df
+
+
+def deal_with_nan(df, action=None):
+    '''
+
+    :param df: Dataframe we want to remove NaN from
+    :param action: Type of action. 'drop_rows' if we want to drop rows with at least one NaN.
+    Choose "impute" if you want to impute the NaN values
+    :return: The Dataframe without NaN's
+    '''
+    if action == "drop_rows":
+        df = df.dropna(axis=0)
+        print("dropping row which contains nan's ", df.shape[0])
+
+    elif action == "impute":
+        df = DataFrameImputer(fill_type="mean_mode").fit_transform(X=df)
+        print("imputing with mode and mean")
+
+    return df
+
+def get_best_grid_cv_model(clf, param_grid, X_train, y_train):
+    '''
+
+    :param clf: Sklearn classifier(such as RandomForestClassifier, GradientBoostingClassifier, etc)
+    :param param_grid: Object of type dictionary:
+
+        param_grid = {
+        "n_estimators": [200, 300],
+        "max_depth": [20, 25],
+        "min_samples_leaf": [3, 5],
+        'min_samples_split': [5, 10],
+        'bootstrap': [True, False]
+    }
+
+    :param X_train: Dataframe without target variable
+    :param y_train: Target variable
+
+    :return: A classifier instance with the optimal parameters for the current dataset and parameters grid
+    '''
+
+    cv_grid_cv = GridSearchCV(estimator=clf, param_grid=param_grid, verbose=10)
+    cv_grid_cv.fit(X_train, y_train)
+
+    print(cv_grid_cv.best_estimator_.get_params())
+
+    params = {k: cv_grid_cv.best_estimator_.get_params()[k] for k in param_grid}
+
+    clf = clf(**params)
+
+    return clf
+
+
+# Categorical DataFrame imputer
+# https://stackoverflow.com/questions/25239958/impute-categorical-missing-values-in-scikit-learn?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+
+
+class DataFrameImputer(TransformerMixin):
+
+    def __init__(self, fill_type="mean_mode"):
+        '''
+
+        :param fill_type: Should be 'mean_mode' to impute numerical columns with mean and categorical columns with mode.
+        Should be 'const' if we want to impute with a constant value(usually -1)
+        '''
+        self.fill_type = fill_type
+
+    def fit(self, X, y=None):
+        '''
+
+        :param X: Dataframe we want to impute
+        :return: Filled dataframe
+        '''
+
+        if self.fill_type == "mean_mode":
+            self.fill = pd.Series([X[c].value_counts().index[0]
+                if X[c].dtype == np.dtype('O') else X[c].mean() for c in X],
+                index=X.columns)
+
+        elif self.fill_type == "const":
+            self.fill = pd.Series(["-1" if X[c].dtype == np.dtype('O') else -1 for c in X],
+                                  index=X.columns)
+
+        return self
+
+    def transform(self, X, y=None):
+        return X.fillna(self.fill)
