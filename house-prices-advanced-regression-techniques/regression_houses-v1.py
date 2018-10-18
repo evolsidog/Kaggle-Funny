6653ad7af5a816aab5e032aa224ec32f79a6832a
Víctor Cordero <victorcordero100@gmail.com>
2018-10-18 17:21:26 +0200

    Add requirements and histogram of target variable

--- house-prices-advanced-regression-techniques/regression_houses-v1.py
+++ house-prices-advanced-regression-techniques/regression_houses-v1.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 from sklearn import ensemble
-from sklearn.model_selection import train_test_split
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import cross_val_score
 import os
 import time
 import pandas_profiling
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
 
 SEED = 1234
 USER = "victor"
@@ -13,27 +15,52 @@ PATH = os.path.join(os.path.sep, "home", USER, ".kaggle", "house-prices-advanced
 # ------ Load train and test ----- #
 
 ini_time = time.time()
-train_path = os.path.join(PATH, 'train.csv')
-test_path = os.path.join(PATH, 'test.csv')
 
-train_df = pd.read_csv(train_path, parse_dates=True, encoding='UTF-8')
-test_df = pd.read_csv(test_path, parse_dates=True, encoding='UTF-8')
+
+def load_data():
+    train_path = os.path.join(PATH, 'train.csv')
+    test_path = os.path.join(PATH, 'test.csv')
+
+    train_df = pd.read_csv(train_path, parse_dates=True, encoding='UTF-8')
+    test_df = pd.read_csv(test_path, parse_dates=True, encoding='UTF-8')
+
+    return train_df, test_df
+
+
+def get_shapes(df):
+    dims = train_df.shape
+    print("Rows: " + str(dims[0]) + "\nColumns: "+ str(dims[1]))
+
+
+def plot_target_histogram(y, num_bins=25, name='target'):
+    fig, ax = plt.subplots()
+    ax.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
+    ax.hist(y, num_bins, facecolor='blue', alpha=0.7)
+    ax.xaxis.set_major_locator(MaxNLocator(15))
+    ax.yaxis.set_major_locator(MaxNLocator(15))
+    # plt.hist(np.log1p(y), bins=num_bins, facecolor='blue', alpha=0.7)
+    fig.savefig('hist_' + name + '.png')
+
+
+train_df, test_df = load_data()
 
 # ------ EDA ------ #
-print(train_df.describe())
-print(train_df.corr())
-print(train_df.shape)
+# print(train_df.describe())
+# print(train_df.corr())
+get_shapes(train_df)
+plot_target_histogram(train_df['SalePrice'])
 
-profile = pandas_profiling.ProfileReport(train_df)
-profile.to_file(outputfile="profiling_V1.html")
-rejected_variables = profile.get_rejected_variables(threshold=0.7)
-print(rejected_variables)
+# profile = pandas_profiling.ProfileReport(train_df)
+# profile.to_file(outputfile="profiling_V1.html")
+# rejected_variables = profile.get_rejected_variables(threshold=0.7)
+# print(rejected_variables)
 
 # ------ Workaround ----- #
 numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
 fake_train_df = train_df.select_dtypes(include=numerics)
 fake_train_df = fake_train_df.fillna(method='pad')
-print(fake_train_df.shape)
+get_shapes(fake_train_df)
+
 
 
 Y = fake_train_df['SalePrice']
